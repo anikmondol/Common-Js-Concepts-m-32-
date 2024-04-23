2ed51e390672da84eab6fef65ecae47f9bbb71bc
anikmondol <anikmondol558363@gmil.com>
2024-04-23 21:19:08 +0600

    different trutht and falsy value in javascript

---
+++ true-false.js
@@ -0,0 +1,51 @@
+/*
+
+Truth:
+1. true
+2. any number (+ve, -ve) will be truthy other than (0)
+3. any string other than empty string
+4. '0', 'false'
+5. []
+6. {}
+
+
+
+
+
+
+Falsy:
+1. false
+2. 0
+3. '' (empty string)
+4. undefined
+5. null
+*/ 
+
+
+
+
+// let x = {};
+
+// if(x){
+//     console.log('value of x is truthy');
+// }else{
+//     console.log('value of x is falsy');
+// }
+
+
+// optional
+
+const x = false;
+
+if(!x){
+    // console.log(false);
+}
+
+
+// -----------------
+
+const z = [];
+
+if(!!z){
+    console.log(true);
+}
