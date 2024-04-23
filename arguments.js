aff8408ace7900ffd0cc2239d6df9773dd98cdc4
anikmondol <anikmondol558363@gmil.com>
2024-04-24 02:12:05 +0600

    update more module 32

--- arguments.js
+++ arguments.js
@@ -1,21 +1,47 @@
-// ------> array like object
+// // ------> array like object
 
 
 
-function sum(a, b, c, d){
-    // console.log(arguments);
-    // console.log(arguments[5]);
-    // console.log(typeof arguments);
-    // for(let i = 1; i < arguments.length; i++){
-    //     console.log(arguments[i]);
-    // }
-    // const args = [...arguments];
-    // console.log(args);
+// function sum(a, b, c, d){
+//     // console.log(arguments);
+//     // console.log(arguments[5]);
+//     // console.log(typeof arguments);
+//     // for(let i = 1; i < arguments.length; i++){
+//     //     console.log(arguments[i]);
+//     // }
+//     // const args = [...arguments];
+//     // console.log(args);
 
-    const results = a + b + c + d;
-    return results;
-}
+//     const results = a + b + c + d;
+//     return results;
+// }
 
 // sum(10, 12, 33, 44, 88, 55, 74);
 
-console.log(typeof sum);
+// console.log(typeof sum);
+
+// ------------------------------
+
+
+// const sum =(p, q)=> {
+//     p + q;
+//    }
+//    const result = sum(2, 3);
+//    console.log(result);
+
+
+// -------------------
+
+// if ("2" === 2) {
+//     console.log("Inside if");
+//    } else {
+//     console.log("Inside else");
+//    }
+
+
+// ------------------------
+
+function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32);
