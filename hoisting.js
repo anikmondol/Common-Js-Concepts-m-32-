e3dedb96ceab9400f65f549dddb13d5e5bd03049
anikmondol <anikmondol558363@gmil.com>
2024-04-23 22:17:23 +0600

    block scope, global scope simple understanding of hoisting

---
+++ hoisting.js
@@ -0,0 +1,20 @@
+// print5();
+print10();
+for(let i = 0; i < 5; i ++){
+    // console.log(i);
+}
+
+// console.log('outSide', i);
+
+// ------------------------
+
+function print5(){
+    // console.log(5);
+}
+
+
+// ---------------------
+
+var print10 =  function (){
+    console.log(10);
+}
